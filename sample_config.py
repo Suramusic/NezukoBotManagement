1ef676c590194b2b0ee21a244ddf15bb53a58584
Suramusic <123370264+Suramusic@users.noreply.github.com>
2023-03-04 06:38:53 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -12,11 +12,11 @@ SUDO_USERS_ID = [int(x) for x in environ.get("SUDO_USERS_ID", "1471469091").spli
 LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", "-1001849137304"))
 GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", "-1001849137304"))
 MESSAGE_DUMP_CHAT = int(environ.get("MESSAGE_DUMP_CHAT", "-1001849137304"))
-WELCOME_DELAY_KICK_SEC = int(environ.get("WELCOME_DELAY_KICK_SEC", "300"))
+WELCOME_DELAY_KICK_SEC = int(environ.get("WELCOME_DELAY_KICK_SEC", "3"))
 MONGO_URL = environ.get("MONGO_URL", "mongodb+srv://Tamilbots:tamilbots@cluster0.aa2e1nd.mongodb.net/?retryWrites=true&w=majority")
-ARQ_API_URL = environ.get("ARQ_API_URL", None)
+ARQ_API_URL = environ.get("ARQ_API_URL", "https://thearq.tech")
 ARQ_API_KEY = environ.get("ARQ_API_KEY", "GNNJTA-QHWNIV-YLMCTS-YPBLST-ARQ")
-RSS_DELAY = int(environ.get("RSS_DELAY", None))
+RSS_DELAY = int(environ.get("RSS_DELAY", 300))
 UPSTREAM_REPO = environ.get(
     "UPSTREAM_REPO", "https://github.com/rozari0/NezukoBot.git"
 )
