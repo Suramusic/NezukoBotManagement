79f95cfd71bdfcfdececfa990fcc7e8150c1a7c0
Suramusic <123370264+Suramusic@users.noreply.github.com>
2023-03-04 06:37:38 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -5,17 +5,17 @@ from dotenv import load_dotenv
 if path.exists("config.env"):
     load_dotenv("config.env")
 
-BOT_TOKEN = environ.get("BOT_TOKEN", None)
-API_ID = int(environ.get("API_ID", 6))
-API_HASH = environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-SUDO_USERS_ID = [int(x) for x in environ.get("SUDO_USERS_ID", "").split()]
-LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", None))
-GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", None))
-MESSAGE_DUMP_CHAT = int(environ.get("MESSAGE_DUMP_CHAT", None))
-WELCOME_DELAY_KICK_SEC = int(environ.get("WELCOME_DELAY_KICK_SEC", None))
-MONGO_URL = environ.get("MONGO_URL", None)
+BOT_TOKEN = environ.get("BOT_TOKEN", "5948498614:AAGldqJ9CgnMzfbEX5Tp2WSXYUkXG5qJSE4")
+API_ID = int(environ.get("API_ID", "28420275"))
+API_HASH = environ.get("API_HASH", "ee41d63cbcf69ab63a3514642785a349")
+SUDO_USERS_ID = [int(x) for x in environ.get("SUDO_USERS_ID", "1471469091").split()]
+LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", "-1001849137304"))
+GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", "-1001849137304"))
+MESSAGE_DUMP_CHAT = int(environ.get("MESSAGE_DUMP_CHAT", "-1001849137304"))
+WELCOME_DELAY_KICK_SEC = int(environ.get("WELCOME_DELAY_KICK_SEC", "300"))
+MONGO_URL = environ.get("MONGO_URL", "mongodb+srv://Tamilbots:tamilbots@cluster0.aa2e1nd.mongodb.net/?retryWrites=true&w=majority")
 ARQ_API_URL = environ.get("ARQ_API_URL", None)
-ARQ_API_KEY = environ.get("ARQ_API_KEY", None)
+ARQ_API_KEY = environ.get("ARQ_API_KEY", "GNNJTA-QHWNIV-YLMCTS-YPBLST-ARQ")
 RSS_DELAY = int(environ.get("RSS_DELAY", None))
 UPSTREAM_REPO = environ.get(
     "UPSTREAM_REPO", "https://github.com/rozari0/NezukoBot.git"
