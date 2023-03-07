e9e101dd7a8f492cf06776dd580c2027f440869b
Suramusic <123370264+Suramusic@users.noreply.github.com>
2023-03-07 19:03:47 +0530

    Update repo.py

--- nezuko/modules/repo.py
+++ nezuko/modules/repo.py
@@ -45,3 +45,4 @@ async def repo(_, message):
     text = f"""[Owner](https://t.me/thavarajtj) | [Channel](t.me/Alinallmovies)
     await app.send_message(
         message.chat.id, text=text, disable_web_page_preview=True
+    )
