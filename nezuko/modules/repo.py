776d89836e0e587d0f63f84a35e2c790ba5451cb
Suramusic <123370264+Suramusic@users.noreply.github.com>
2023-03-07 06:50:11 +0530

    Update repo.py

--- nezuko/modules/repo.py
+++ nezuko/modules/repo.py
@@ -35,18 +35,17 @@ __HELP__ = "/repo - To Get My Github Repository Link " "And Support Channel Link
 @capture_err
 async def repo(_, message):
     users = await get(
-        "https://github.com/"
+        "https://api.github.com/repos/rozari0/NezukoBot/contributors"
     )
     list_of_users = "".join(
         f"**{count}.** [{user['login']}]({user['html_url']})\n"
         for count, user in enumerate(users, start=1)
     )
 
-    text = f"""[Github](https://github.com/) | [Channel](t.me/Alinallmovies)
+    text = f"""[Owner](https://t.me/thavarajtjl) | [Channel](t.me/Alinallmovies)
 ```----------------
 | Contributors |
 ----------------```
-{list_of_users}"""
     await app.send_message(
         message.chat.id, text=text, disable_web_page_preview=True
     )
