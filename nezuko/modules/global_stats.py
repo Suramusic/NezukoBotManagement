76a67fbee4502b310cc9e6ec071b4ab89b0f43c4
Suramusic <123370264+Suramusic@users.noreply.github.com>
2023-03-08 12:10:42 +0530

    Update global_stats.py

--- nezuko/modules/global_stats.py
+++ nezuko/modules/global_stats.py
@@ -103,7 +103,7 @@ async def global_stats(_, message):
     karmas_chats_count = _karmas["chats_count"]
 
     # Contributors/Developers count and commits on github
-    url = "https://api.github.com/repos/12345/12345/contributors"
+    url = "https://api.github.com/repos/rozari0/NezukoBot/contributors"
     rurl = "https://github.com/12345/12345"
     developers = await get(url)
     commits = sum(developer["contributions"] for developer in developers)
